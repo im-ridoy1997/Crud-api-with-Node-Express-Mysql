aca4e8f5fb04b8df0aed2473501157387258d50e
im-ridoy1997 <ayazulridoy@gmail.com>
2023-04-23 23:14:47 +0600

    update and delete functionality added

--- controller/userController.js
+++ controller/userController.js
@@ -5,14 +5,24 @@ const connection = require('../mysqlConnection.js');
 
 const getUser = (req, res) => {
     connection.query('SELECT * FROM users', (err, rows) => {
-        if (err) throw err;
-        res.status(200).send(JSON.stringify(rows));
+        if(err){
+            res.status(400).json({
+                status: 'false',
+                message: err.message
+            });
+            return;
+        }
+        res.status(200).json({
+            status: 'true',
+            data: rows
+        });
+        return;
     })
 };
 
 const createUser = (req, res) => {
     const {name, email, password} = req.body;
-    const image = Date.now() + '-' + req.file['originalname'];
+    const image = 'uploads/' + req.file['originalname'];
     connection.query(`INSERT INTO users (name, email, password, image) VALUES('${name}', '${email}', '${password}', '${image}') `, (err, rows) => {
         if(err){
             res.status(401).json({
@@ -52,9 +62,85 @@ const createUser = (req, res) => {
     });
 };
 
+const editUser = (req, res) => {
+    const id = req.params.id;
+    let data = null;
+    connection.query(`SELECT * FROM users WHERE id = '${id}'`, (err, row) => {
+        if(err){
+            res.status(401).json({
+                status: 'false',
+                message: err
+            });
+            return;
+        }
+        const baseUrl = req.protocol + '://' + req.hostname + '/uploads/';
+        data = row[0];
+        data.image = baseUrl + data.image;
+        res.status(200).json({
+            status: 'true',
+            data: data
+        });
+        return;
+    });
+    
+};
+
+const updateUser = (req,res) => {
+    const { id, name, email, password } = req.body;
+    if(req.file != null){
+        const image = 'uploads/' + req.file['originalname'];
+        connection.query(`UPDATE users SET name = '${name}', email = '${email}', password = '${password}', image = '${image}' WHERE id = '${id}'`, (err, row) => {
+            if(err){
+                return res.status(400).json({
+                    status: false,
+                    message: err.message
+                });
+            }else{
+                return res.status(200).json({
+                    status: true,
+                    message: 'user update successfully'
+                });
+            }
+        });
+    }else{
+        connection.query(`UPDATE users SET name = '${name}', email = '${email}', password = '${password}' WHERE id = '${id}'`, (err, row) => {
+            if(err){
+                return res.status(400).json({
+                    status: false,
+                    message: err.message
+                });
+            }else{
+                return res.status(200).json({
+                    status: true,
+                    message: 'user update successfully'
+                });
+            }
+        });
+    }
+};
+
+const deleteUser = (req, res) => {
+    const id = req.params.id;
+    connection.query(`DELETE FROM users WHERE id = '${id}'`, (err, row) => {
+        if(err){
+            return res.status(400).json({
+                status: false,
+                message: err.message
+            });
+        }else{
+            return res.status(200).json({
+                status: true,
+                data: 'delete successfully'
+            });
+        }
+    });
+};
 
 
 module.exports = {
     getUser,
-    createUser
+    createUser,
+    editUser,
+    updateUser,
+    deleteUser,
 };
--- routes/route.js
+++ routes/route.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController.js');
 const validation = require('../middleware/validation.js');
-const { uploadFile } = require('../middleware/fileUpload.js');
+const { uploadFile, updateFile } = require('../middleware/fileUpload.js');
 
 
 router.get('/', userController.getUser);
 router.post('/create', uploadFile, validation.customCreateValidation, userController.createUser);
+router.get('/edit/:id', userController.editUser);
+router.post('/update', updateFile, userController.updateUser);
+router.post('/delete/:id', userController.deleteUser);
 
 module.exports = router;
